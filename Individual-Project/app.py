91b37c031ea08b6fdae5f4c323ae8988f3708a4d
avigaille23-meet <avigaillevite@gmail.com>
2022-08-14 19:19:41 +0300

    hi aaaa

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -5,12 +5,31 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+Config = {
+  "apiKey": "AIzaSyD0n72GG_1GhyEqLY7OOYXE1DCWVtQlh5M",
+  "authDomain": "scouts-a9024.firebaseapp.com",
+  "projectId": "scouts-a9024",
+  "storageBucket": "scouts-a9024.appspot.com",
+  "messagingSenderId": "133652738723",
+  "appId": "1:133652738723:web:2c64edd438df47c23f4516",
+  "measurementId": "G-QLJVDVM9WT"
+  "dataURL":"",
+}
 
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+	
 
+@app.route('/singin')
+def singin():
+	return render_template("home.html")
 
+@app.route('/singup')
+def singup():
+	return render_template("singup.html")
+	
+	
 
-#Code goes above here
 
 if __name__ == '__main__':
     app.run(debug=True)
